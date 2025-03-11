d880e5f1d345cb845695b0b33be4a74dbb0254ad
Euan Jed Tabamo <estabamo1@up.edu.ph>
2025-03-11 16:34:03 +0800

    docs: add initial documentation to controller.js

--- controller.js
+++ controller.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI = "mongodb://127.0.0.1:27017/StudentDatabase";
-
+// Connect to local MongoDB server
 try {
 	await mongoose.connect(MONGODB_URI);
 	console.log("Connected to MongoDB");
 } catch (error) {
 	console.log(error);
 }
-
+// Create a Student model
 const Student = mongoose.model("Student", {
 	stdnum: String,
 	fname: String,
@@ -16,11 +16,22 @@ const Student = mongoose.model("Student", {
 	age: Number,
 });
 
+// Saves a student to the database
 const saveStudent = (req, res) => {};
-const updateStudentFirstName = (req, res) => {};
-const removeUser = (req, res) => {};
-const removeAllUsers = (req, res) => {};
-const getUser = (req, res) => {};
+
+// Updates the first name of a student in the database
+const updateStudentFirstName = (req, res) => {}; 
+
+// Removes a specific user from the database
+const removeUser = (req, res) => {}; 
+
+// Removes all users from the database (dangerous)
+const removeAllUsers = (req, res) => {}; 
+
+// Retrieves a user from the database
+const getUser = (req, res) => {}; 
+
+// Retrives all users from the database
 const getMembers = (req, res) => {};
 
 export {
